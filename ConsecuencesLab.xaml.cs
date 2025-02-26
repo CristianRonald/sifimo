cd9faa476ac53104c8b9afa612097656f6e8cfca
Cristian Ronald <67212011+CristianRonald@users.noreply.github.com>
2025-02-26 00:58:40 -0500

    Subida de archivos

---
+++ ConsecuencesLab.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace sifimo
+{
+    /// <summary>
+    /// Lógica de interacción para ConsecuencesLab.xaml
+    /// </summary>
+    public partial class ConsecuencesLab : UserControl
+    {
+        private DispatcherTimer timer;
+        private TimeSpan tiempo, tiempo_r;
+        private bool _active = false;
+
+        public static readonly DependencyProperty ImageSourceProperty =
+        DependencyProperty.Register("ImageSource", typeof(string), typeof(ConsecuencesLab), new PropertyMetadata(null));
+        double velocidadNaveSpacial = 5;
+        public string ImageSource
+        {
+            get { return (string)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+
+        public ConsecuencesLab()
+        {
+            InitializeComponent();
+
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tiempo = tiempo.Add(TimeSpan.FromSeconds(1));
+            tiempo_imput.Text = tiempo.ToString(@"hh\:mm\:ss\:fff");
+            double _ = TiempoRelativo(tiempo.TotalSeconds, velocidad.Value);
+            tiempo_r = TimeSpan.FromSeconds(TiempoRelativo(tiempo.TotalSeconds, velocidad.Value));
+            tiempo_relativo.Text = tiempo_r.ToString(@"hh\:mm\:ss\:fff");
+        }
+        private static double Gamma(double v)
+        {
+            return (1 / Math.Sqrt(1 - (v * v)));
+        }
+        private double TiempoRelativo(double t, double v)
+        {
+            return t / Gamma(v);
+        }
+        private double MasaRelativo(int m, double v)
+        {
+            double masa = m * Gamma(v);
+            double aux = ((masa * 100) / m) - 100;
+            navespacial.Margin = new Thickness(0, aux * 1.5, 0, 0);
+            return masa;
+        }
+        private double LargoRelativo(double L, double v)
+        {
+            double large = L / Gamma(v);
+            navespacial.Height = (large * 100) / L;
+            return large;
+        }
+
+        private void ActiveSimulacion()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            tiempo = TimeSpan.Zero;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            AnimateImage(velocidadNaveSpacial);
+            textButton.Text = "Parar";
+        }
+        private void DesactiveSimulacion()
+        {
+            textButton.Text = "Simular";
+            tiempo = TimeSpan.Zero;
+            tiempo_r = TimeSpan.Zero;
+            timer.Stop();
+            navespacial.RenderTransform.BeginAnimation(TranslateTransform.XProperty, null);
+            //tiempo_imput.Text = tiempo.ToString(@"hh\:mm\:ss");
+            //tiempo_relativo.Text = "00:00:00";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                BorderMasa.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FBFFE4"));
+                BorderLarge.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FBFFE4"));
+                if (string.IsNullOrWhiteSpace(masa.Text))
+                {
+                    BorderMasa.BorderBrush = new SolidColorBrush(Colors.Red);
+                }
+                if (string.IsNullOrWhiteSpace(largeInput.Text))
+                {
+                    BorderLarge.BorderBrush = new SolidColorBrush(Colors.Red);
+                }
+                if (!_active && !string.IsNullOrWhiteSpace(masa.Text) && !string.IsNullOrWhiteSpace(largeInput.Text))
+                {
+                    ActiveSimulacion();
+                    _active = true;
+                }
+                else if (_active)
+                {
+                    DesactiveSimulacion();
+                    _active = false;
+                }
+                 
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void velocidad_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            VelocidadLuz.Text = $"{velocidad.Value:F1}c";
+            velocidadNaveSpacial = 5;
+            if (velocidad.Value > 0) velocidadNaveSpacial = 0.4 / velocidad.Value;
+            //MessageBox.Show(velocidadNaveSpacial.ToString());
+
+            if (_active)
+            {
+                navespacial.RenderTransform.BeginAnimation(TranslateTransform.XProperty, null);
+                
+                masa_relative.Text = $"m = {MasaRelativo(Int32.Parse(masa.Text), velocidad.Value):F2}kg";
+                large_ralative.Text = $"L = {LargoRelativo(Double.Parse(largeInput.Text), velocidad.Value):F2}m";
+            }
+            if(!_active) AnimateImage(velocidadNaveSpacial);
+        }
+        private void FinalizarSimulacion(object sender, EventArgs e ) 
+        {
+            MessageBox.Show("La simulacion ha finalizado.");
+            DesactiveSimulacion();
+        }
+        private void AnimateImage(double v)
+        {
+
+            TranslateTransform transform = new TranslateTransform();
+            navespacial.RenderTransform = transform;
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = 0,
+                To = 690,
+                Duration = TimeSpan.FromSeconds(v)
+            };
+            animation.Completed += FinalizarSimulacion;
+            transform.BeginAnimation(TranslateTransform.XProperty, animation);   
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sifimo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Presentacion ps = new Presentacion();
+            ps.PresentacionChanged += OpenConsecuencias;
+            Main.Children.Add(ps);
+        }
+        private void OpenConsecuencias(object? sender, EventArgs e) 
+        { 
+            Main.Children.Clear();
+            ConsecuencesLab consecuences = new();
+            consecuences.ImageSource = "Images/navespacial.png";
+            Main.Children.Add(consecuences);
+        }
+    }
+}
